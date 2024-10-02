9217a70c64ebe1c3fa6b2b07be65b2f9741a9c50
JeevanJoshi4434 <jeetbhaiya25@gmail.com>
2024-10-02 22:50:25 +0530

    API support for frontend

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function sendMessage() {
         messageInput.value = '';
 
         //send the msg to flask backend
-        fetch('http://127.0.0.1:5000/chat',{
+        fetch('https://chat-bot-vsbx.onrender.com/chat',{
             method: 'POST',
             headers: {
                 'Content-Type' : 'application/json'
